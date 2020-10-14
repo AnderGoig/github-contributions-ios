28c25122e9d5d47ebb26b9105d33d1c60104877c
Ander Goig <goig.ander@gmail.com>
2020-10-14 22:48:35 +0200

    Initial commit

---
+++ GitHubContributions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubContributions
+//
+//  Created by Ander Goig on 14/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubContributions/GitHubContributionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubContributionsApp.swift
+//  GitHubContributions
+//
+//  Created by Ander Goig on 14/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GitHubContributionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
