f74274d3159a5b54a2797ee5ba779273717da59d
Ander Goig <goig.ander@gmail.com>
2020-10-26 00:43:05 +0100

    Use remote images

--- GitHubContributions/Guide/StepView.swift
+++ GitHubContributions/Guide/StepView.swift
@@ -6,32 +6,39 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct StepView: View {
 
+    @Environment (\.colorScheme) var colorScheme: ColorScheme
     let step: Step
 
     var body: some View {
-        VStack {
+        VStack(spacing: 32.0) {
             VStack(alignment: .center, spacing: 6.0) {
                 Text(step.title)
                     .font(.system(size: 28.0, weight: .bold, design: .default))
                     .foregroundColor(.primary)
 
                 Text(step.subtitle)
-                    .font(.body)
+                    .font(.callout)
                     .foregroundColor(.secondary)
                     .multilineTextAlignment(.center)
+                    .lineLimit(2)
             }
 
-            Image(step.image)
+            KFImage(colorScheme == .dark ? step.darkImage : step.lightImage, options: [.backgroundDecode])
+                .placeholder {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                }
                 .resizable()
                 .scaledToFit()
-                .offset(y: 60.0)
-                .scaleEffect(1.5)
         }
-        .padding(.top, 40.0)
-        .padding(.horizontal)
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding(.horizontal, 24.0)
+        .padding(.bottom, 64.0)
+        .padding(.top, 32.0)
     }
 
 }
